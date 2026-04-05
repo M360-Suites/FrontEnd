9d30cb631b913837473e494f2198374883cf8179
Olutunmise Adeniji <tadeniji06@gmail.com>
2026-04-05 20:27:39 +0100

    ---

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,2 +1,2 @@
 export const devApiUrl = "http://localhost:6000/api";
-export const prodApiUrl = "https://dm360-app-wwuho.ondigitalocean.app/api";
+export const prodApiUrl = "https://server.biz360prime.com/api";
