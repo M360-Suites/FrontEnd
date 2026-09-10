25962a91bd4292c666041b7c24110c153dfe0530
Olutunmise Adeniji <tadeniji06@gmail.com>
2026-09-10 15:38:29 +0100

    ---

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.6gnihqh9n3k"
+    "revision": "0.e08fgtvdd2s"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
